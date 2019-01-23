973c28b433237367394aad74a112af4c9fa9610a
diana <diana3372@gmail.com>
2019-01-23 16:52:26 +0100

    Refactor to spit files

---
+++ model.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.distributions.categorical import Categorical
+
+class Sender(nn.Module):
+	def __init__(self, n_image_features, vocab_size, 
+		embedding_dim, hidden_size, batch_size):
+		super().__init__()
+
+		self.batch_size = batch_size
+		self.hidden_size = hidden_size
+		self.lstm_cell = nn.LSTMCell(embedding_dim, hidden_size)
+		self.aff_transform = nn.Linear(n_image_features, hidden_size)
+		self.embedding = nn.Embedding(vocab_size, embedding_dim)
+		self.linear_probs = nn.Linear(hidden_size, vocab_size) # from a hidden state to the vocab
+
+	def forward(self, t, start_token_idx, max_sentence_length, greedy=True):
+		message = torch.zeros([self.batch_size, max_sentence_length], dtype=torch.long)
+
+		# h0, c0, w0
+		h = self.aff_transform(t) # batch_size, hidden_size
+		c = torch.zeros([self.batch_size, self.hidden_size])
+		w = self.embedding(torch.ones([self.batch_size], dtype=torch.long) * start_token_idx)
+
+		for i in range(max_sentence_length): # or sampled <S>, but this is batched
+			h, c = self.lstm_cell(w, (h, c))
+
+			p = F.softmax(self.linear_probs(h), dim=1)
+
+			if self.training or not self.greedy:
+				cat = Categorical(p)
+				w_idx = cat.sample() # rsample?
+			else:
+				_, w_idx = torch.max(p, -1)
+
+			message[:,i] = w_idx
+
+			# For next iteration
+			w = self.embedding(torch.LongTensor(w_idx))
+
+		return message, cat.log_prob(w_idx)
+
+
+class Receiver(nn.Module):
+	def __init__(self, n_image_features, vocab_size,
+		embedding_dim, hidden_size, batch_size):
+		super().__init__()
+
+		self.batch_size = batch_size
+		self.hidden_size = hidden_size
+		self.lstm_cell = nn.LSTMCell(embedding_dim, hidden_size)
+		self.embedding = nn.Embedding(vocab_size, embedding_dim)
+		self.aff_transform = nn.Linear(hidden_size, n_image_features)
+
+	def forward(self, m):
+		# h0, c0
+		h = torch.zeros([self.batch_size, self.hidden_size])
+		c = torch.zeros([self.batch_size, self.hidden_size])
+
+		# Need to change to batch dim second to iterate over tokens in message
+		m = m.permute(1, 0)
+
+		for w_idx in m:
+			emb = self.embedding(w_idx)
+			h, c = self.lstm_cell(emb, (h, c))
+
+		return self.aff_transform(h)
+
+
+class Model(nn.Module):
+	def __init__(self, n_image_features, vocab_size,
+		embedding_dim, hidden_size, batch_size):
+		super().__init__()
+
+		self.sender = Sender(n_image_features, vocab_size,
+			embedding_dim, hidden_size, batch_size)
+		self.receiver = Receiver(n_image_features, vocab_size,
+			embedding_dim, hidden_size, batch_size)
+
+	def forward(self, target, distractors, word_to_idx, start_token, max_sentence_length):
+		m, log_prob = self.sender(target, word_to_idx[start_token], max_sentence_length)
+
+		r_transform = self.receiver(m) # g(.)
+
+		loss = 0
+		r_transform = r_transform.permute(1,0)
+
+		for d in distractors:
+			loss += torch.max(torch.tensor(0.0), 1.0 - target @ r_transform + d @ r_transform)
+
+		loss = -loss * log_prob
+		
+		return torch.mean(loss)
--- test.py
+++ test.py
@@ -3,15 +3,11 @@ import numpy as np
 import random
 
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from torch.distributions.categorical import Categorical
-
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import BatchSampler
 
 from ImageDataset import ImageDataset, ImagesSampler
-
+from model import Sender, Receiver, Model
 from utils import AverageMeter
 
 EPOCHS = 2#1000
@@ -21,88 +17,6 @@ BATCH_SIZE = 128
 MAX_SENTENCE_LENGTH = 5#13
 START_TOKEN = '<S>'
 K = 2 # number of distractors
-
-
-class Sender(nn.Module):
-	def __init__(self, n_image_features, vocab_size):
-		super().__init__()
-
-		self.lstm_cell = nn.LSTMCell(EMBEDDING_DIM, HIDDEN_SIZE)
-		self.aff_transform = nn.Linear(n_image_features, HIDDEN_SIZE)
-		self.embedding = nn.Embedding(vocab_size, EMBEDDING_DIM)
-		self.linear_probs = nn.Linear(HIDDEN_SIZE, vocab_size) # from a hidden state to the vocab
-
-	def forward(self, t, start_token_idx, greedy=True):
-		message = torch.zeros([BATCH_SIZE, MAX_SENTENCE_LENGTH], dtype=torch.long)
-
-		# h0, c0, w0
-		h = self.aff_transform(t) # BATCH_SIZE, HIDDEN_SIZE
-		c = torch.zeros([BATCH_SIZE, HIDDEN_SIZE])
-		w = self.embedding(torch.ones([BATCH_SIZE], dtype=torch.long) * start_token_idx)
-
-		for i in range(MAX_SENTENCE_LENGTH): # or sampled <S>, but this is batched
-			h, c = self.lstm_cell(w, (h, c))
-
-			p = F.softmax(self.linear_probs(h), dim=1)
-
-			if self.training or not self.greedy:
-				cat = Categorical(p)
-				w_idx = cat.sample() # rsample?
-			else:
-				_, w_idx = torch.max(p, -1)
-
-			message[:,i] = w_idx
-
-			# For next iteration
-			w = self.embedding(torch.LongTensor(w_idx))
-
-		return message, cat.log_prob(w_idx)
-
-
-class Receiver(nn.Module):
-	def __init__(self, n_image_features, vocab_size):
-		super().__init__()
-
-		self.lstm_cell = nn.LSTMCell(EMBEDDING_DIM, HIDDEN_SIZE)
-		self.embedding = nn.Embedding(vocab_size, EMBEDDING_DIM)
-		self.aff_transform = nn.Linear(HIDDEN_SIZE, n_image_features)
-
-	def forward(self, m):
-		# h0, c0
-		h = torch.zeros([BATCH_SIZE, HIDDEN_SIZE])
-		c = torch.zeros([BATCH_SIZE, HIDDEN_SIZE])
-
-		# Need to change to batch dim second to iterate over tokens in message
-		m = m.permute(1, 0)
-
-		for w_idx in m:
-			emb = self.embedding(w_idx)
-			h, c = self.lstm_cell(emb, (h, c))
-
-		return self.aff_transform(h)
-
-
-class Model(nn.Module):
-	def __init__(self, n_image_features, vocab_size):
-		super().__init__()
-
-		self.sender = Sender(n_image_features, vocab_size)
-		self.receiver = Receiver(n_image_features, vocab_size)
-
-	def forward(self, target, distractors, word_to_idx):
-		m, log_prob = self.sender(target, word_to_idx[START_TOKEN])
-
-		r_transform = self.receiver(m) # g(.)
-
-		loss = 0
-		r_transform = r_transform.permute(1,0)
-
-		for d in distractors:
-			loss += torch.max(torch.tensor(0.0), 1.0 - target @ r_transform + d @ r_transform)
-
-		loss = -loss * log_prob
-		
-		return torch.mean(loss)
 
 
 # Load data
@@ -130,7 +44,8 @@ valid_data = DataLoader(valid_dataset, num_workers=8, pin_memory=True,
 
 
 # Settings
-model = Model(n_image_features, vocab_size)
+model = Model(n_image_features, vocab_size,
+	EMBEDDING_DIM, HIDDEN_SIZE, BATCH_SIZE)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
@@ -151,9 +66,8 @@ for e in range(EPOCHS):
 
 		target, distractors = d
 	
-		loss = model(target, distractors, word_to_idx)
+		loss = model(target, distractors, word_to_idx, START_TOKEN, MAX_SENTENCE_LENGTH)
 
-		print(loss.item())
 		epoch_loss_meter.update(loss.item())
 
 		loss.backward()
