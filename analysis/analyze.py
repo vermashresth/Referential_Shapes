0ea99996e783318ef912fd6e573ec47d4ce2a336
diana <diana3372@gmail.com>
2019-02-21 21:46:21 +0100

    Computing stats

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -1,13 +1,16 @@
 import sys
 import os
 import pickle
+import numpy as np
 
-from utils import get_message_length
+from utils import compute_stats, plot_token_distribution, get_attributes
 
 
 assert len(sys.argv) == 2, 'You need a model id'
 
 model_id = sys.argv[1]
+
+data_folder = 'balanced'
 
 dumps_dir = '../dumps'
 model_dir = '{}/{}'.format(dumps_dir, model_id)
@@ -23,8 +26,21 @@ x = pickle.load(open(messages_file_name, 'rb'))
 # First token is always <S>
 padding_token = x[0][0]
 
-
 # Grab stats
+min_len, max_len, avg_len, counter = compute_stats(x, padding_token)
 
+print('Min message length:', min_len)
+print('Max message length:', max_len)
+print('Avg message length:', avg_len)
+n_utt = len(counter)
+print('Number of used tokens', n_utt)
+common_percent = 0.3
+print('Top 30 percent most used tokens', counter.most_common(int(n_utt * common_percent)))
 
-print(get_message_length(x[0], padding_token))
+plot_token_distribution(counter, model_id)
+
+metadata = pickle.load(open('../shapes/{}/test.metadata.p'.format(data_folder), 'rb'))
+# print(len(metadata))
+# print(metadata[0])
+
+dict = get_attributes(messages, padding_token, metadata)
--- analysis/utils.py
+++ analysis/utils.py
@@ -1,17 +1,14 @@
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
 def get_message_length(message, padding_token):
-
-	message[-1] = padding_token
-	message[-2] = padding_token
-	message[-3] = padding_token
-
-	print(message)
-
-
 	assert message[0] == padding_token
 	
 	pad_idx = 0
 
 	for i in reversed(range(len(message))):
+		assert False, 'Need to check if message[i] I need to do .item() or padding_token is what???'
 		if message[i] == padding_token:
 			pad_idx = i
 		else:
@@ -22,4 +19,75 @@ def get_message_length(message, padding_token):
 		return len(message) - 1
 	else:
 		return pad_idx - 1
+
+
+def compute_stats(messages, padding_token):
+	max_len = -1
+	min_len = len(messages[0])
+	avg_len = 0
+
+	c = Counter()
+
+	for i,m in enumerate(messages):		
+		length = get_message_length(m, padding_token)
+		if length > max_len:
+			max_len = length
+		if length < min_len:
+			min_len = length
+		avg_len = avg_len*((i)/(i+1)) + length*(1/(i+1))
+
+		for token in m:
+			c[token.item()] += 1
+
+	return min_len, max_len, avg_len, c
+
+
+def plot_token_distribution(counter, model_id):
+	n_tokens_limit = 500
+	tokens = []
+	occurrences = []
+
+	if len(counter) > n_tokens_limit:
+		for k,v in counter.most_common(n_tokens_limit):
+			tokens.append(str(k))
+			occurrences.append(v)
+	else:
+		for k,v in counter.most_common():
+			tokens.append(str(k))
+			occurrences.append(v)
+
+	# Frequency rank
+	plt.title('Tokens distribution{}'.format(' (partial)' if len(counter) > n_tokens_limit else ''))
+	plt.ylabel('Number of occurrences')
+	plt.xlabel('Token')
+	plt.plot(tokens, occurrences)
+
+	plt.savefig('token_dist_{}.png'.format(model_id))
+
+def get_feature_ids(message_metadata):
+	ids = []
+	for k, v_list in message_metadata.items():
+		flat_list = [item for sublist in v_list for item in sublist]
+		for v in flat_list:
+			if not v == None:
+				ids.append('{}_{}'.format(k[:-1], v))
+
+	return ids
+
+def get_attributes(messages, padding_token, metadata):
+	dict = {}
+
+	for i, m in enumerate(messages):
+		for t in m:
+			if t == padding_token: ## Is this correct? or need .item() ?
+				continue
+
+			token = t.item()
+			if not token in dict:
+				dict[token] = Counter()
+			feature_keys = get_feature_ids(metadata[i])
+			for feature_key in feature_keys:
+				dict[token][feature_key] += 1
+
+	return dict
 
