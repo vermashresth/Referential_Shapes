ac1e0f0466ac88694a134ad6507b41f0b462f9eb
diana <diana3372@gmail.com>
2019-02-22 15:02:49 +0100

    Created plots, fixed errors

--- analysis/analyze.py
+++ analysis/analyze.py
@@ -2,45 +2,99 @@ import sys
 import os
 import pickle
 import numpy as np
+import pandas as pd
 
-from utils import compute_stats, plot_token_distribution, get_attributes
+from utils import *
 
 
-assert len(sys.argv) == 2, 'You need a model id'
+def get_stats(model_id, vocab_size, data_folder, plots_dir):
+	_, idx_to_word, padding_idx = load_dictionaries(vocab_size)
 
-model_id = sys.argv[1]
+
+	dumps_dir = '../dumps'
+	model_dir = '{}/{}'.format(dumps_dir, model_id)
+
+	accuracy_file_name = ['{}/{}'.format(model_dir, f) for f in os.listdir(model_dir) if 'test_accuracy_meter.p' in f]
+	assert len(accuracy_file_name) == 1
+	accuracy_file_name = accuracy_file_name[0]
+	acc_meter = pickle.load(open(accuracy_file_name, 'rb'))
+
+	messages_file_name = ['{}/{}'.format(model_dir, f) for f in os.listdir(model_dir) if 'test_messages.p' in f]
+
+	assert len(messages_file_name) == 1
+	messages_file_name = messages_file_name[0]
+
+	# Load messages
+	messages = pickle.load(open(messages_file_name, 'rb'))
+
+	# Grab stats
+	min_len, max_len, avg_len, counter = compute_stats(messages, padding_idx, idx_to_word)
+
+	n_utt = len(counter)
+
+	# Plots
+	top_common_percent = 0.3
+	plot_token_frequency(counter, n_utt, top_common_percent, model_id, plots_dir)
+	plot_token_distribution(counter, model_id, plots_dir)
+
+	# Token correlation with features
+	metadata = pickle.load(open('../shapes/{}/test.metadata.p'.format(data_folder), 'rb'))
+
+	token_to_attr, attr_to_token = get_attributes_dicts(messages, padding_idx, metadata, idx_to_word)
+
+	plot_attributes_per_token(counter, n_utt, top_common_percent, token_to_attr, model_id, plots_dir)
+	n_top_tokens = 10
+	plot_tokens_per_attribute(attr_to_token, n_top_tokens, model_id, plots_dir)
+
+
+	return (len(messages[0]), 
+			acc_meter.avg,
+			min_len,
+			max_len,
+			avg_len,
+			n_utt)
+
+
+
+
+assert len(sys.argv) >= 3 and len(sys.argv) % 2 != 0, 'You need at least one model id and its vocabulary size'
+
+
+plots_dir = 'plots'
+
+if not os.path.exists(plots_dir):
+	os.mkdir(plots_dir)
 
 data_folder = 'balanced'
+stats_dict = {
+	'id': [],
+	'|V|' : [],
+	'L' : [],
+	'Test acc': [],
+	'Min message length': [],
+	'Max message length' : [],
+	'Avg message length': [],
+	'N tokens used': []
+}
 
-dumps_dir = '../dumps'
-model_dir = '{}/{}'.format(dumps_dir, model_id)
+for i in range(1, len(sys.argv), 2):
+	model_id = sys.argv[i]
+	vocab_size = sys.argv[i+1]
 
-messages_file_name = ['{}/{}'.format(model_dir, f) for f in os.listdir(model_dir) if 'test_messages.p' in f]
+	L, acc, min_len, max_len, avg_len, n_utt = get_stats(model_id, vocab_size, data_folder, plots_dir)
 
-assert len(messages_file_name) == 1
-messages_file_name = messages_file_name[0]
+	stats_dict['id'].append(model_id)
+	stats_dict['|V|'].append(vocab_size)
+	stats_dict['L'].append(L)
+	stats_dict['Test acc'].append(acc)
+	stats_dict['Min message length'].append(min_len)
+	stats_dict['Max message length'].append(max_len)
+	stats_dict['Avg message length'].append(avg_len)
+	stats_dict['N tokens used'].append(n_utt)
 
-# Load messages
-x = pickle.load(open(messages_file_name, 'rb'))
 
-# First token is always <S>
-padding_token = x[0][0]
 
-# Grab stats
-min_len, max_len, avg_len, counter = compute_stats(x, padding_token)
+df = pd.DataFrame(stats_dict)
+df.to_csv('{}/stats_{}.csv'.format(plots_dir, data_folder), index=None, header=True)
 
-print('Min message length:', min_len)
-print('Max message length:', max_len)
-print('Avg message length:', avg_len)
-n_utt = len(counter)
-print('Number of used tokens', n_utt)
-common_percent = 0.3
-print('Top 30 percent most used tokens', counter.most_common(int(n_utt * common_percent)))
 
-plot_token_distribution(counter, model_id)
-
-metadata = pickle.load(open('../shapes/{}/test.metadata.p'.format(data_folder), 'rb'))
-# print(len(metadata))
-# print(metadata[0])
-
-dict = get_attributes(messages, padding_token, metadata)
--- analysis/utils.py
+++ analysis/utils.py
@@ -1,27 +1,35 @@
 from collections import Counter
 import matplotlib.pyplot as plt
+import pickle
 
+def load_dictionaries(vocab_size):
+	with open("../data/shapes/dict_{}.pckl".format(vocab_size), "rb") as f:
+	    d = pickle.load(f)
+	    word_to_idx = d["word_to_idx"] # dictionary w->i
+	    idx_to_word = d["idx_to_word"] # list of words
+	    bound_idx = word_to_idx["<S>"] # last word in vocab
 
-def get_message_length(message, padding_token):
-	assert message[0] == padding_token
+	return word_to_idx, idx_to_word, bound_idx
+
+def get_message_length(message, padding_idx):
+	assert message[0] == padding_idx, 'First token should always be <S>!'
 	
-	pad_idx = 0
+	start_pad_pos = 0
 
 	for i in reversed(range(len(message))):
-		assert False, 'Need to check if message[i] I need to do .item() or padding_token is what???'
-		if message[i] == padding_token:
-			pad_idx = i
+		if message[i] == padding_idx:
+			start_pad_pos = i
 		else:
 			break
 
 	# we ignore the first token for statistical purposes
-	if pad_idx == 0:
+	if start_pad_pos == 0:
 		return len(message) - 1
 	else:
-		return pad_idx - 1
+		return start_pad_pos - 1
 
 
-def compute_stats(messages, padding_token):
+def compute_stats(messages, padding_idx, idx_to_word):
 	max_len = -1
 	min_len = len(messages[0])
 	avg_len = 0
@@ -29,40 +37,139 @@ def compute_stats(messages, padding_token):
 	c = Counter()
 
 	for i,m in enumerate(messages):		
-		length = get_message_length(m, padding_token)
+		length = get_message_length(m, padding_idx)
 		if length > max_len:
 			max_len = length
 		if length < min_len:
 			min_len = length
 		avg_len = avg_len*((i)/(i+1)) + length*(1/(i+1))
 
-		for token in m:
-			c[token.item()] += 1
+		for token_idx in m:
+			c[idx_to_word[token_idx]] += 1
 
 	return min_len, max_len, avg_len, c
 
 
-def plot_token_distribution(counter, model_id):
+def plot_token_distribution(counter, model_id, plots_dir):
 	n_tokens_limit = 500
 	tokens = []
 	occurrences = []
 
 	if len(counter) > n_tokens_limit:
-		for k,v in counter.most_common(n_tokens_limit):
-			tokens.append(str(k))
-			occurrences.append(v)
+		items = counter.most_common(n_tokens_limit)
 	else:
-		for k,v in counter.most_common():
-			tokens.append(str(k))
-			occurrences.append(v)
+		items = counter.most_common()
+	
+	for k,v in items:
+		tokens.append(k)
+		occurrences.append(v)
 
 	# Frequency rank
+	plt.clf()
 	plt.title('Tokens distribution{}'.format(' (partial)' if len(counter) > n_tokens_limit else ''))
 	plt.ylabel('Number of occurrences')
 	plt.xlabel('Token')
+	plt.xticks(rotation=90)
 	plt.plot(tokens, occurrences)
 
-	plt.savefig('token_dist_{}.png'.format(model_id))
+	plt.savefig('{}/token_dist_{}.png'.format(plots_dir, model_id))
+
+
+def plot_token_frequency(counter, n_utterances, percent, model_id, plots_dir):
+	tokens = []
+	occurrences = []
+
+	for k, v in counter.most_common(int(n_utterances * percent)):
+		tokens.append(k)
+		occurrences.append(v)
+
+	plt.clf()
+	plt.title('Frequency for top {}% most used tokens'.format(percent * 100))
+	plt.ylabel('Number of occurrences')
+	plt.xlabel('Token')
+	plt.xticks(rotation=90)
+	plt.bar(tokens, occurrences)
+
+	plt.savefig('{}/top_{}percent_most_freq_{}.png'.format(plots_dir, int(percent*100), model_id))
+
+def plot_attributes_per_token(counter, n_utterances, percent, token_to_attr, model_id, plots_dir):
+	tokens = []
+
+	for token, _ in counter.most_common(int(n_utterances * percent)):
+		if token == '<S>':
+			continue
+		tokens.append(token)
+
+	plt.clf()
+	plt.title('Attributes for top {}% most used tokens'.format(int(percent * 100)))
+	plt.ylabel('Attributes')
+	plt.xlabel('Token')
+	plt.xticks(rotation=90)
+
+	data_shape0 = [token_to_attr[t]['shape_0'] for t in tokens]
+	data_shape1 = [token_to_attr[t]['shape_1'] for t in tokens]
+	data_shape2 = [token_to_attr[t]['shape_2'] for t in tokens]
+	data_color0 = [token_to_attr[t]['color_0'] for t in tokens]
+	data_color1 = [token_to_attr[t]['color_1'] for t in tokens]
+	data_color2 = [token_to_attr[t]['color_2'] for t in tokens]
+	data_size0 = [token_to_attr[t]['size_0'] for t in tokens]
+	data_size1 = [token_to_attr[t]['size_1'] for t in tokens]
+
+	pshape0 = plt.bar(tokens, data_shape0)
+	pshape1 = plt.bar(tokens, data_shape1, bottom=data_shape0)
+	pshape2 = plt.bar(tokens, data_shape2, bottom=data_shape1)
+	pcolor0 = plt.bar(tokens, data_color0, bottom=data_shape2)
+	pcolor1 = plt.bar(tokens, data_color1, bottom=data_color0)
+	pcolor2 = plt.bar(tokens, data_color2, bottom=data_color1)
+	psize0 = plt.bar(tokens, data_size0, bottom=data_color2)
+	psize1 = plt.bar(tokens, data_size1, bottom=data_size0)
+
+	plt.legend(
+		(pshape0[0], pshape1[0], pshape2[0], pcolor0[0], pcolor1[0], pcolor2[0], psize0[0], psize1[0]), 
+		('circle', 'square', 'triangle', 'red', 'green', 'blue', 'small', 'big'))
+
+	plt.savefig('{}/attributes_per_top_{}p_tokens_{}.png'.format(plots_dir, int(percent*100), model_id))
+
+
+def plot_tokens_per_attribute(attr_to_token, n_top_tokens, model_id, plots_dir):
+	plt.clf()
+	plt.figure(figsize=(12,9)) # Originally 8x6 (inches)
+	plt.title('Top {} tokens per attribute'.format(n_top_tokens))
+
+	# 8 attributes
+	n_rows = 4
+	n_columns = 2
+	pos = 1
+
+	attrs_strings = {'shape_0': 'circle',
+					 'shape_1': 'square',
+					 'shape_2': 'triangle',
+					 'color_0': 'red',
+					 'color_1': 'green',
+					 'color_2': 'blue',
+					 'size_0': 'small',
+					 'size_1': 'big'}
+
+	for attr_id, title in attrs_strings.items():
+		c = attr_to_token[attr_id]
+		tokens = []
+		occurrences = []
+		for token, v in c.most_common(n_top_tokens):
+			if token == '<S>':
+				continue
+
+			tokens.append(token)
+			occurrences.append(v)
+
+		plt.subplot(n_rows, n_columns, pos, title=title)
+		pos += 1
+		plt.bar(tokens, occurrences)
+		plt.ylabel('Occurrences')
+		plt.xlabel('Tokens')
+		plt.xticks(rotation=90)
+
+	plt.savefig('{}/top_{}_tokens_per_attribute.png'.format(plots_dir, n_top_tokens))
+
 
 def get_feature_ids(message_metadata):
 	ids = []
@@ -74,20 +181,47 @@ def get_feature_ids(message_metadata):
 
 	return ids
 
-def get_attributes(messages, padding_token, metadata):
-	dict = {}
+def get_attributes_dicts(messages, padding_idx, metadata, idx_to_word):
+	token_to_attr = {}
+	attr_to_token = {}
 
 	for i, m in enumerate(messages):
-		for t in m:
-			if t == padding_token: ## Is this correct? or need .item() ?
+		feature_keys = get_feature_ids(metadata[i])
+
+		for token_idx in m:
+			if token_idx == padding_idx:
 				continue
 
-			token = t.item()
-			if not token in dict:
-				dict[token] = Counter()
-			feature_keys = get_feature_ids(metadata[i])
+			token = idx_to_word[token_idx]
+			if not token in token_to_attr:
+				token_to_attr[token] = Counter()
+			
 			for feature_key in feature_keys:
-				dict[token][feature_key] += 1
+				token_to_attr[token][feature_key] += 1
 
-	return dict
+				if not feature_key in attr_to_token:
+					attr_to_token[feature_key] = Counter()
 
+				attr_to_token[feature_key][token] += 1
+
+	return token_to_attr, attr_to_token
+
+class AverageMeter:
+    def __init__(self):
+        self.value = None
+        self.avg = None
+        self.sum = None
+        self.count = None
+        self.reset()
+
+    def reset(self):
+        self.value = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, value, n=1):
+        self.value = value
+        self.sum += value * n
+        self.count += n
+        self.avg = self.sum / self.count
