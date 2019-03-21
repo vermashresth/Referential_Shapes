2e5ed9da198bf7d5a4d3294ec120c4a66f68c7f1
diana <diana3372@gmail.com>
2019-03-21 14:50:01 +0100

    Add pad UTs

--- ut_model.py
+++ ut_model.py
@@ -10,7 +10,7 @@ class TestModel(unittest.TestCase):
     def setUpClass(self):
         self.model = Model(n_image_features=4096, vocab_size=3,
             embedding_dim=256, hidden_size=512, batch_size=2, 
-            bound_idx=9, max_sentence_length=5, use_gpu=False)
+            bound_idx=2, max_sentence_length=5, use_gpu=False)
 
     def test_get_word_counts_train(self):
         self.model.train()
@@ -42,16 +42,84 @@ class TestModel(unittest.TestCase):
         self.model.eval()
 
         m = torch.tensor([
-                [
-                    [2, 1, 0, 0, 2, 2]
-                ],
-                [
-                    [2, 0, 1, 0, 1, 0]
-                ]
+                [2, 1, 0, 0, 2, 2],
+                [2, 0, 1, 0, 1, 0]
             ])
 
         res = self.model._get_word_counts(m)
 
         self.assertTrue(torch.all(torch.eq(res, torch.tensor([5, 3, 4]))))
+
+    def test_pad_train(self):
+        self.model.train()
+
+        m = torch.tensor([
+                [
+                    [0.0, 0.0, 1.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                ],
+                [
+                    [0.0, 0.0, 1.0],
+                    [0.0, 1.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                ]
+            ])
+
+        seq_lengths = torch.tensor([4, 6])
+
+        res = self.model._pad(m, seq_lengths)
+
+        self.assertTrue(torch.all(
+            torch.eq(
+                res,
+                torch.tensor([
+                    [
+                        [0.0, 0.0, 1.0],
+                        [1.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0],
+                        [0.0, 0.0, 1.0],
+                        [0.0, 0.0, 1.0],
+                        [0.0, 0.0, 1.0],
+                    ],
+                    [
+                        [0.0, 0.0, 1.0],
+                        [0.0, 1.0, 0.0],
+                        [1.0, 0.0, 0.0],
+                        [1.0, 0.0, 0.0],
+                        [0.0, 1.0, 0.0],
+                        [0.0, 1.0, 0.0],
+                    ]
+                ])
+            )))
+
+    def test_pad_eval(self):
+        self.model.eval()
+
+        m = torch.tensor([
+                [2, 1, 0, 0, 2, 0],
+                [2, 0, 1, 0, 1, 0]
+            ])
+
+        seq_lengths = torch.tensor([5, 6])
+
+        res = self.model._pad(m, seq_lengths)
+
+        self.assertTrue(torch.all(
+            torch.eq(
+                res,
+                    torch.tensor([
+                    [2, 1, 0, 0, 2, 2],
+                    [2, 0, 1, 0, 1, 0]
+                ])
+            )))
+
+
 
     
