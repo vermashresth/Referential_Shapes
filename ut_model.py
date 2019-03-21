d6a84063cc6dd59a35d8438479a02ba638a49718
diana <diana3372@gmail.com>
2019-03-21 11:11:25 +0100

    Add UTs

---
+++ ut_model.py
@@ -0,0 +1,57 @@
+import unittest
+import torch
+
+from model import Model
+
+
+class TestModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.model = Model(n_image_features=4096, vocab_size=3,
+            embedding_dim=256, hidden_size=512, batch_size=2, 
+            bound_idx=9, max_sentence_length=5, use_gpu=False)
+
+    def test_get_word_counts_train(self):
+        self.model.train()
+
+        m = torch.tensor([
+                [
+                    [0.0, 0.0, 1.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, 1.0],
+                    [0.0, 0.0, 1.0],
+                    [0.0, 0.0, 1.0],
+                ],
+                [
+                    [0.0, 0.0, 1.0],
+                    [0.0, 1.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                ]
+            ])
+
+        res = self.model._get_word_counts(m)
+
+        self.assertTrue(torch.all(torch.eq(res, torch.tensor([3, 4, 5]))))
+
+    def test_get_word_counts_eval(self):
+        self.model.eval()
+
+        m = torch.tensor([
+                [
+                    [2, 1, 0, 0, 2, 2]
+                ],
+                [
+                    [2, 0, 1, 0, 1, 0]
+                ]
+            ])
+
+        res = self.model._get_word_counts(m)
+
+        self.assertTrue(torch.all(torch.eq(res, torch.tensor([5, 3, 4]))))
+
+    
---
+++ ut_sender.py
@@ -0,0 +1,76 @@
+import unittest
+import torch
+
+from model import Sender
+
+
+class TestSender(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.sender = Sender(n_image_features=4096, vocab_size=3, 
+            embedding_dim=256, hidden_size=512, batch_size=2, 
+            bound_idx=2, max_sentence_length=3,
+            use_gpu=False, greedy=True)
+
+    def test_calculate_seq_len_train(self):
+        self.sender.train()
+
+        initial_length = self.sender.max_sentence_length + 1
+        seq_lengths = torch.ones([self.sender.batch_size], dtype=torch.int64) * initial_length
+
+        timestep = 1
+        token = torch.tensor([
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, 1.0]
+            ])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, False)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([initial_length, 2]))))
+
+        timestep = 2
+        token = torch.tensor([
+                [1.0, 0.0, 0.0],
+                [0.1, 0.0, 0.0]
+            ])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, False)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([initial_length, 2]))))
+
+        timestep = 3
+        token = torch.tensor([
+                [1.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0]
+            ])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, False)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([initial_length, 2]))))
+
+
+    def test_calculate_seq_len_eval(self):
+        self.sender.eval()
+
+        initial_length = self.sender.max_sentence_length + 1
+        seq_lengths = torch.ones([self.sender.batch_size], dtype=torch.int64) * initial_length
+
+        timestep = 1
+        token = torch.tensor([1, 2])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, True)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([initial_length, 2]))))
+
+        timestep = 2
+        token = torch.tensor([2, 2])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, True)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([3, 2]))))
+
+        timestep = 3
+        token = torch.tensor([2, 0])
+
+        self.sender._calculate_seq_len(seq_lengths, token, initial_length, timestep, True)
+        self.assertTrue(torch.all(torch.eq(seq_lengths, torch.tensor([3, 2]))))
+
+    
+
+    
