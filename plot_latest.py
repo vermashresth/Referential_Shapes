1bb89b59150fc1fefa3725ef61eff864018f6ab3
diana <diana3372@gmail.com>
2019-03-21 17:55:20 +0100

    Fix plotting code

--- plot_latest.py
+++ plot_latest.py
@@ -4,30 +4,21 @@ import pickle
 import os
 
 assert len(sys.argv) == 3, 'Input dumps folder/model id, file id'
-dumps_folder, model_id = sys.argv[1].split('/')
+folder = sys.argv[1]
+model_id, _, _ = folder.split('/')[1].split('_')
 file_id = 'losses_meters' if 'loss' in sys.argv[2] else ('accuracy_meters' if 'acc' in sys.argv[2] else None)
 
-#01_25_08_07_92_losses_meters.p
-
-folder = '{}/{}'.format(dumps_folder, model_id)
+#01_25_08_07_92_losses_meters.p # Not anymore
+#0314200052021692_46_accuracy_meters.p
 
 # Retrieve latest epoch
-file_names = ['{}/{}'.format(folder,f) for f in os.listdir(folder) if 'model' in f]
+file_names = ['{}/{}'.format(folder,f) for f in os.listdir(folder) if '{}.p'.format(file_id) in f]
 file_names.sort(key=os.path.getctime)
 
-latest_model_file_name = file_names[-1]
-last_underscore = latest_model_file_name.rfind('_')
-second_last_underscore = latest_model_file_name[:last_underscore].rfind('_')
-epoch = int(latest_model_file_name[second_last_underscore+1:last_underscore])
+print(file_names)
+assert len(file_names) == 2 and 'eval' in file_names[-1]
 
-
-train_file = '{}/{}_{}_{}.p'.format(folder, model_id, epoch, file_id)
-val_file = '{}/{}_{}_eval_{}.p'.format(folder, model_id, epoch, file_id)
-
-if not os.path.exists(train_file):
-    epoch -= 1
-    train_file = '{}/{}_{}_{}.p'.format(folder, model_id, epoch, file_id)
-    val_file = '{}/{}_{}_eval_{}.p'.format(folder, model_id, epoch, file_id)
+train_file, val_file = file_names
 
 train_data = pickle.load(open(train_file,'rb'))
 val_data = pickle.load(open(val_file,'rb'))
@@ -44,5 +35,5 @@ metric = file_id[:file_id.rfind('_')]
 plt.ylabel('Avg {}'.format(metric))
 plt.title('{} curves'.format(metric))
 #plt.show()
-fig_id = '{}_{}'.format(model_id, epoch)
+fig_id = '{}'.format(model_id)
 plt.savefig('{}/{}_curves_{}.png'.format(folder, file_id, fig_id))
