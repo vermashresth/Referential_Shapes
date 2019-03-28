c18e691b3383032e10f804dc463254940e01e67b
diana <diana3372@gmail.com>
2019-03-28 15:21:18 +0100

    Just verifying dataset generation

--- shapes/visualize.py
+++ shapes/visualize.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import pickle
 
 def display_image(img):
 	plt.clf()
@@ -20,17 +21,24 @@ def display_image(img):
 
 
 
-folder = 'different_targets'
+folder = 'dummy'#'different_targets'
 npy_file = '{}/train.large.input.npy'.format(folder)
+metadata_file = '{}/train.large.metadata.p'.format(folder)
 
 assert len(sys.argv) > 1, 'Indicate an index!'
 
-img = np.load(npy_file)[int(sys.argv[1])]
+index = int(sys.argv[1])
+
+img = np.load(npy_file)[index]
 print(img.shape)
+
+metadata = pickle.load(open(metadata_file, 'rb'))[index]
 
 if len(img.shape) == 4: # We have tuples because this is 4-D
 	n_images = img.shape[0]
 	for i in range(n_images):
 		display_image(img[i])
+		print(metadata[i])
 else:
 	display_image(img)
+	print(metadata)
