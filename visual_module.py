16fe3067717eaaf520b49077b7d7e9dfa004f985
diana <diana3372@gmail.com>
2019-03-28 19:43:07 +0100

    In progress of changing vgg16

--- visual_module.py
+++ visual_module.py
@@ -2,19 +2,30 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+
 class CNN(nn.Module):
 	def __init__(self, use_gpu):
 		super().__init__()
 
-		self.vgg16 = models.vgg16(pretrained=False)
-		if use_gpu:
-			self.vgg16 = self.vgg16.cuda()
+		
 
 	def forward(self, x):
-		y = self.vgg16.features(x)
-		y = y.view(y.size(0), -1)
-		y = self.vgg16.classifier[:5](y)
-		return y
+		
+
+# vgg16
+# class CNN(nn.Module):
+# 	def __init__(self, use_gpu):
+# 		super().__init__()
+
+# 		self.vgg16 = models.vgg16(pretrained=False)
+# 		if use_gpu:
+# 			self.vgg16 = self.vgg16.cuda()
+
+# 	def forward(self, x):
+# 		y = self.vgg16.features(x)
+# 		y = y.view(y.size(0), -1)
+# 		y = self.vgg16.classifier[:5](y)
+# 		return y
 
 
 
