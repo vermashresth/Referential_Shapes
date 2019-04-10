2fa3c4f3c43fb289676506781f2de2faeeea6158
diana <diana3372@gmail.com>
2019-04-10 15:07:36 +0200

    Add meters for new metrics: RSA and topological similarity

--- run.py
+++ run.py
@@ -1,19 +1,18 @@
-from utils import AverageMeter
+from utils import AverageMeter, discretize_messages
 import torch
 import torch.nn as nn
 
-
-def discretize_messages(m):
-	_, res = torch.max(m, dim=-1)
-	return res
-
-def run_epoch(model, data, word_counts, optimizer, debugging):
+def run_epoch(model, data, word_counts, optimizer, onehot_metadata, debugging):
 	is_training_mode = not optimizer is None
 
 	loss_meter = AverageMeter()
 	acc_meter = AverageMeter()
 	entropy_meter = AverageMeter()
 	distinctness_meter = AverageMeter()
+	rsa_sr_meter = AverageMeter()
+	rsa_si_meter = AverageMeter()
+	rsa_ri_meter = AverageMeter()
+	topological_sim_meter = AverageMeter()
 	messages = []
 	indices = []
 	w_counts = word_counts.clone()
@@ -24,12 +23,26 @@ def run_epoch(model, data, word_counts, optimizer, debugging):
 
 		target, distractors, idxs = d
 
-		loss, acc, m, batch_w_counts, entropy, distinctness = model(target, distractors, w_counts)
+		(loss,
+		acc, 
+		m, 
+		batch_w_counts, 
+		entropy, 
+		distinctness,
+		rsa_sr,
+		rsa_si,
+		rsa_ri,
+		topological_sim) = model(target, distractors, w_counts, onehot_metadata[idxs[:,0]])
 
 		loss_meter.update(loss.item())
 		acc_meter.update(acc.item())
 		entropy_meter.update(entropy.item())
 		distinctness_meter.update(distinctness)
+		rsa_sr_meter.update(rsa_sr)
+		rsa_si_meter.update(rsa_si)
+		rsa_ri_meter.update(rsa_ri)
+		topological_sim_meter.update(topological_sim)
+
 		messages.append(discretize_messages(m) if is_training_mode else m)
 		indices.append(idxs)
 		w_counts += batch_w_counts
@@ -47,13 +60,17 @@ def run_epoch(model, data, word_counts, optimizer, debugging):
 		torch.cat(indices, 0), 
 		w_counts, 
 		entropy_meter,
-		distinctness_meter)
+		distinctness_meter,
+		rsa_sr_meter,
+		rsa_si_meter,
+		rsa_ri_meter,
+		topological_sim_meter)
 
 
-def train_one_epoch(model, data, optimizer, word_counts, debugging=False):
+def train_one_epoch(model, data, optimizer, word_counts, onehot_metadata, debugging=False):
 	model.train()
-	return run_epoch(model, data, word_counts, optimizer, debugging)
+	return run_epoch(model, data, word_counts, optimizer, onehot_metadata, debugging)
 
-def evaluate(model, data, word_counts, debugging=False):
+def evaluate(model, data, word_counts, onehot_metadata, debugging=False):
 	model.eval()
-	return run_epoch(model, data, word_counts, None, debugging)
+	return run_epoch(model, data, word_counts, None, onehot_metadata, debugging)
