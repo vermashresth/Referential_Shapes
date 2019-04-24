3d986b344531eee3673afd54bfe61bcf41e4b1e1
diana <diana3372@gmail.com>
2019-04-24 14:48:45 +0200

    Add the possibility to train with symbolic input

--- ImageDataset.py
+++ ImageDataset.py
@@ -109,3 +109,60 @@ class ImageFeaturesDataset():
 
     def __len__(self):
         return self.features.shape[0]
+
+
+class ImageFeaturesDatasetZeroShot():
+    def __init__(self, target_features, distractors_features, mean=None, std=None):
+        if mean is None:
+            mean = (np.mean(target_features, axis=0) + np.mean(distractors_features, axis=0)) / 2
+            std = (np.std(target_features, axis=0) + np.std(distractors_features, axis=0)) / 2
+            std[np.nonzero(std == 0.0)] = 1.0  # nan is because of dividing by zero
+        self.mean = mean
+        self.std = std
+        
+        self.target_features = (target_features - self.mean) / (2 * self.std)
+        self.distractors_features = (distractors_features - self.mean) / (2 * self.std)
+
+    def __getitem__(self, indices):        
+        target_idx = indices[0]
+        distractors_idxs = indices[1:]
+        
+        distractors = []
+        for d_idx in distractors_idxs:
+            distractors.append(self.distractors_features[d_idx])
+
+        return (self.target_features[target_idx], distractors, indices)
+
+    def __len__(self):
+        return self.target_features.shape[0]
+
+
+class ImagesSamplerZeroShot(Sampler):
+    def __init__(self, data_source, k, shuffle):
+        self.n = len(data_source)
+        self.k = k
+        self.shuffle = shuffle
+        assert self.k < self.n
+
+    def __iter__(self):
+        indices = []
+
+        if self.shuffle:
+            targets = torch.randperm(self.n).tolist()
+        else:
+            targets = list(range(self.n))
+
+        for t in targets:
+            arr = np.zeros(self.k + 1, dtype=int) # distractors + target
+            arr[0] = t
+            distractors = random.sample(range(self.n), self.k)
+            # while t in distractors:
+            #     distractors = random.sample(range(self.n), self.k)
+            arr[1:] = np.array(distractors)
+            
+            indices.append(arr)
+
+        return iter(indices)
+
+    def __len__(self):
+        return self.n
--- dataloader.py
+++ dataloader.py
@@ -3,7 +3,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import BatchSampler
 
-from ImageDataset import ImageDataset, ImageFeaturesDataset, ImagesSampler
+from ImageDataset import ImageDataset, ImageFeaturesDataset, ImagesSampler, ImageFeaturesDatasetZeroShot, ImagesSamplerZeroShot
 
 def load_dictionaries(folder, vocab_size):
 	with open("data/{}/dict_{}.pckl".format(folder, vocab_size), "rb") as f:
@@ -36,14 +36,15 @@ def load_images(folder, batch_size, k):
 
 
 # This is for loading previously obtained features
-def load_pretrained_features(folder, batch_size, k):
-	train_features = np.load('{}/train_features.npy'.format(folder))
-	valid_features = np.load('{}/valid_features.npy'.format(folder))
-	test_features = np.load('{}/test_features.npy'.format(folder))
-	# train_features = np.load('data/{}/train_features.npy'.format(folder))
-	# valid_features = np.load('data/{}/valid_features.npy'.format(folder))
-	# test_features = np.load('data/{}/test_features.npy'.format(folder))
-	# 2d arrays of 4096 features
+def load_pretrained_features(folder, batch_size, k, use_symbolic=False):
+	if use_symbolic:
+		train_features = np.load('{}/train.large.onehot_metadata.p'.format(folder)).astype(np.float32)
+		valid_features = np.load('{}/val.onehot_metadata.p'.format(folder)).astype(np.float32)
+		test_features = np.load('{}/test.onehot_metadata.p'.format(folder)).astype(np.float32)
+	else:
+		train_features = np.load('{}/train_features.npy'.format(folder))
+		valid_features = np.load('{}/valid_features.npy'.format(folder))
+		test_features = np.load('{}/test_features.npy'.format(folder))
 
 	n_image_features = valid_features.shape[-1] # 4096
 
@@ -59,5 +60,37 @@ def load_pretrained_features(folder, batch_size, k):
 
 	test_data = DataLoader(test_dataset, num_workers=8, pin_memory=True,
 		batch_sampler=BatchSampler(ImagesSampler(test_dataset, k, shuffle=False), batch_size=batch_size, drop_last=False))
+
+	return n_image_features, train_data, valid_data, test_data
+
+
+# This is for loading previously obtained features
+# This needs to grab targets from the unseen dataset and distractors from unseen + seen grabbed uniformly
+def load_pretrained_features_zero_shot(target_folder, distractors_folder, batch_size, k):
+	target_train_features = np.load('{}/train_features.npy'.format(target_folder))
+	target_valid_features = np.load('{}/valid_features.npy'.format(target_folder))
+	target_test_features = np.load('{}/test_features.npy'.format(target_folder))
+
+	distractors_train_features = np.load('{}/train_features.npy'.format(distractors_folder))
+	distractors_valid_features = np.load('{}/valid_features.npy'.format(distractors_folder))
+	distractors_test_features = np.load('{}/test_features.npy'.format(distractors_folder))
+
+	n_image_features = target_valid_features.shape[-1]
+
+	assert target_valid_features.shape[-1] == distractors_valid_features.shape[-1]
+
+
+	train_dataset = ImageFeaturesDatasetZeroShot(target_train_features, distractors_train_features)
+	valid_dataset = ImageFeaturesDatasetZeroShot(target_valid_features, distractors_valid_features, mean=train_dataset.mean, std=train_dataset.std) # All features are normalized with mean and std
+	test_dataset = ImageFeaturesDatasetZeroShot(target_test_features, distractors_test_features, mean=train_dataset.mean, std=train_dataset.std)
+
+	train_data = DataLoader(train_dataset, num_workers=8, pin_memory=True, 
+		batch_sampler=BatchSampler(ImagesSamplerZeroShot(train_dataset, k, shuffle=True), batch_size=batch_size, drop_last=False))
+
+	valid_data = DataLoader(valid_dataset, num_workers=8, pin_memory=True,
+		batch_sampler=BatchSampler(ImagesSamplerZeroShot(valid_dataset, k, shuffle=False), batch_size=batch_size, drop_last=False))
+
+	test_data = DataLoader(test_dataset, num_workers=8, pin_memory=True,
+		batch_sampler=BatchSampler(ImagesSamplerZeroShot(test_dataset, k, shuffle=False), batch_size=batch_size, drop_last=False))
 
 	return n_image_features, train_data, valid_data, test_data
