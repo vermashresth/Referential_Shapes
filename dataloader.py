f9c349a4cdd6906f35fb9e2496db302aad5053d1
Shresth Verma <vermashresth@gmail.com>
2020-10-03 12:10:54 +0530

    clean commit history

--- dataloader.py
+++ dataloader.py
@@ -7,10 +7,10 @@ from ImageDataset import ImageDataset, ImageDatasetSmart, ImageFeaturesDataset, 
 
 def load_dictionaries(folder, vocab_size):
 	with open("data/{}/dict_{}.pckl".format(folder, vocab_size), "rb") as f:
-	    d = pickle.load(f)
-	    word_to_idx = d["word_to_idx"] # dictionary w->i
-	    idx_to_word = d["idx_to_word"] # list of words
-	    bound_idx = word_to_idx["<S>"] # last word in vocab
+		d = pickle.load(f)
+		word_to_idx = d["word_to_idx"] # dictionary w->i
+		idx_to_word = d["idx_to_word"] # list of words
+		bound_idx = word_to_idx["<S>"] # last word in vocab
 
 	return word_to_idx, idx_to_word, bound_idx
 
@@ -20,9 +20,9 @@ def load_images(folder, batch_size, k):
 	test_filename = '{}/test.input.npy'.format(folder)
 	noise_filename = '{}/noise.input.npy'.format(folder)
 	train_metadata = pickle.load(open('{}/train.large.onehot_metadata.p'.format(folder), 'rb'))
-    valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
-    test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
-    noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
+	valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
+	test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
+	noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
 
 	train_dataset = ImageDataset(train_filename)
 	valid_dataset = ImageDataset(valid_filename, mean=train_dataset.mean, std=train_dataset.std) # All features are normalized with mean and std
@@ -89,9 +89,9 @@ def load_pretrained_features(folder, batch_size, k, use_symbolic=False):
 	noise_dataset = ImageFeaturesDataset(noise_features, mean=train_dataset.mean, std=train_dataset.std)
 
 	train_metadata = pickle.load(open('{}/train.large.onehot_metadata.p'.format(folder), 'rb'))
-    valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
-    test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
-    noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
+	valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
+	test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
+	noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
 
 	train_data = DataLoader(train_dataset, num_workers=8, pin_memory=True,
 		batch_sampler=BatchSampler(ImagesSampler(train_dataset, train_metadata, k, shuffle=True), batch_size=batch_size, drop_last=False))
@@ -124,9 +124,9 @@ def load_pretrained_features_zero_shot(target_folder, distractors_folder, batch_
 	assert target_valid_features.shape[-1] == distractors_valid_features.shape[-1]
 
 	train_metadata = pickle.load(open('{}/train.large.onehot_metadata.p'.format(folder), 'rb'))
-    valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
-    test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
-    noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
+	valid_metadata = pickle.load(open('{}/val.onehot_metadata.p'.format(folder), 'rb'))
+	test_metadata = pickle.load(open('{}/test.onehot_metadata.p'.format(folder), 'rb'))
+	noise_metadata = pickle.load(open('{}/noise.onehot_metadata.p'.format(folder), 'rb'))
 
 	train_dataset = ImageFeaturesDatasetZeroShot(target_train_features, distractors_train_features)
 	valid_dataset = ImageFeaturesDatasetZeroShot(target_valid_features, distractors_valid_features, mean=train_dataset.mean, std=train_dataset.std) # All features are normalized with mean and std
